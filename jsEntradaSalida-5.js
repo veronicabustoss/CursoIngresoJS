f5a542b2b28fd214f906be5ebe55cff76f4456d0
veronicabustoss <40859184+veronicabustoss@users.noreply.github.com>
2018-07-05 20:47:43 -0300

    Commit 2 arreglado
    Arreglo del ejercicio 5

--- jsEntradaSalida-5.js
+++ jsEntradaSalida-5.js
@@ -4,11 +4,9 @@ function mostrar()
 {	
 	var nombre;
 	var edad;
-	nombre=prompt("ingrese su nombre");
-	document.getElementById('elNombre').value=nombre;
-	edad=prompt("ingrese su edad");
-	document.getElementById('laEdad').value=edad;
-	alert("Usted se llama " + nombre + " y tiene " + edad +" años ");/*cuando voy a pegar un texto con una variable me tengo que acordar del valor de mas*/
+	nombre=document.getElementById('elNombre').value;
+	edad=document.getElementById('laEdad').value;
+	alert("Usted se llama "+nombre +" y tiene "+edad +" años");
+}	
 
-}
 
