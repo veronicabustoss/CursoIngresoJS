d1750b612ec151e676e5f514d863a99a7e3ee078
veronicabustoss <40859184+veronicabustoss@users.noreply.github.com>
2018-08-06 17:57:50 -0300

    EJERCICIO 7 DEL RECUPERATORIO
    se hizo de otra manera.

--- siete.js
+++ siete.js
@@ -1,91 +1,91 @@
 function mostrar()
-{	 
-	//poner el nombre del mas viejo.
-	//nota del mas chico;
-
-	var nota;
-	var edad;
+{
+	var notas;
+	var edad=0;
 	var sexo;
-	var contador;
+	var contador=0;
 	var acumulador=0;
 	var promedio;
-	var peorNota;
-	var sexoPeorNota;
-	var nombreMasVieje;
-	var edadMasvieje;
-	var masChico;
-	var notaMasChico;
- //le asigne el valor porque si es igual a cero me da que la nota baja es igual a cero, no estaria bien.
+	var notaBaja=11;
+	var sexoBajo;
+	var contadorVaron=0;
+	var edadJoven=9999;
+	var sexoJoven;
+	var notaJoven=0;
+	var edadPrimeraMujer; //se utiliza cuando hay comparaciones el igual a cero.
+	var bandera=0; 
+	var notaPrimeraMujer;
 
 
-
-	while(contador<5)
-	{	
-		contador ++;
-		
-		nombre=prompt("Ingrese su nombre");
-		
-		sexo=prompt("Ingrese su sexo 'f' o 'm'");
-
-		while(sexo!='f' && sexo!='m')
-		{
-			sexo=prompt("Sexo no valido, ingrese su sexo 'f' o 'm'");
-		}
-
+	while(contador<2)
+	{
 		nota=prompt("Ingrese una nota entre 0 y 10");
 		nota=parseInt(nota);
+		contador ++; //no utilizar la variable dos veces si es que la utilizo dentro de un ciclo while.
 
-
-		while(isNaN(nota) || nota<0 || nota>10)
+		while(nota>10 || nota<0)
 		{
-			nota=prompt("Nota no valida, ingrese una nota entre 0 y 10");
-		}
-
-		edad=prompt("Ingrese su edad, entre 0 a 100");
-		edad=parseInt(edad);
-
-		while(isNaN(edad) || edad<0 || edad>100) //IsNaN es que no ingreso un numero
-		{
-			edad=prompt("Edad no valida, ingrese nuevamente");
+			nota=prompt("Nota no valida ingrese nuevamente una nota entre 0 y 10");
 		}
 
 		acumulador=acumulador+nota;
 
-		if(contador==1)
+		edad=prompt("Ingrese su edad")
+		edad=parseInt(edad);
+
+		while(edad<0 && edad>999)
 		{
-			sexoPeorNota=sexo;
-			peorNota=nota;
-			nombreMasVieje=nombre;
-			edadMasvieje=edad;
-			masChico=edad;
-			notaMasChico=nota;
+			edad=prompt("Edad no valida, ingrese nuevamente");
 		}
-		else
+
+		sexo=prompt("Ingrese su sexo 'f' o 'm'");
+
+		while(sexo!="f" && sexo!="m")
 		{
-			if(nota<peorNota)
+			sexo=prompt("Sexo no valido, ingrese nuevamente 'f' o 'm' ");
+		}
+
+		if(nota<notaBaja)
+		{
+			notaBaja=nota;
+			sexoBajo=sexo;
+		}
+		
+			if(edad>18 && sexo=="m" && nota>=6)
 			{
-				peorNota=nota;
-				sexoPeorNota=sexo;
+				contadorVaron ++;
 			}
 
-			if(edad>edadMasvieje)
-			{
-				nombreMasVieje=nombre;
-				edadMasvieje=edad;	
-			}
-			if(edad<masChico)
-			{
-				masChico=edad;
-				notaMasChico=nota;
-			}
-
+		if(edad<edadJoven)
+		{
+			edadJoven=edad;
+			sexoJoven=sexo;
+			notaJoven=nota;
 		}
+
+		if(bandera==0 && sexo=="f")  //revisar luego.
+		{
+			edadPrimeraMujer=edad;
+			notaPrimeraMujer=nota;
+			bandera=1; //cuando encuentre la primera mujer la banedra cambia a 1 para que la nota no cambie en el siguiente ciclo while.
+		}
+		
 
 	}
 
 	promedio=acumulador/contador;
 
-	alert("a) El promedio de las notas totales es " + promedio + "\nb) La nota más baja es " + notaBaja +" y el sexo de esa persona es "+ sexoBajo + "\nc) La cantidad de varones que su nota haya sido mayor o igual a 6 es "+contadorVaron);
+	alert("a) El promedio de las notas totales es "+ promedio +" \nb) La nota mas baja es " + notaBaja  +" y el sexo de esa persona es "+ sexoBajo + " \nc) la cantidad de varones mayores a 18, que su nota a sido mayor o igual a 6 es "+ contadorVaron +" \nd) El sexo y la nota del más joven  es  "+ sexoJoven +" con nota de " + notaJoven );
 
+	// el \n sirve como  un enter,  separando las palabras en fila ordenadas. 
+
+	if(bandera==0)
+	{
+		alert("No se ingresaron mujeres.");
+	}
+	else 
+	{
+		alert("La edad de la primera mujer ingresada es " + edadPrimeraMujer + " con una nota de "+ notaPrimeraMujer);
+	}	
 
 }
